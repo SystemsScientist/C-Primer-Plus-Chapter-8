5761745bc6b3b43869e85863a2c7ee471da39806
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-10-25 06:40:36 -0500

    Updating Listing-8-4

---
+++ Listing-8-4/guess_v3.c
@@ -0,0 +1,26 @@
+// guess_v3.c -- improves on guess_v2.c
+
+#include <stdio.h>
+
+int main(void) {
+
+	int guess = 1;
+	char response;
+
+	printf("Pick an integer from 1 to 100. I will try to guess ");
+	printf("it. \nRespond with a y if my guess is right and with");
+	printf("\nan n if it is wrong.\n");
+	printf("Uh...is your number %d?\n", guess);
+
+	while ((response = getchar()) != 'y') { // get response
+		if (response == 'n')
+			printf("Well, then, is it %d?\n", ++guess);
+		else
+			printf("Sorry, I understand only y or n.\n");
+		while (getchar() != '\n')
+			continue; // skip rest of input line
+	}
+	printf("I knew I could do it!\n");
+
+	return 0;
+}
